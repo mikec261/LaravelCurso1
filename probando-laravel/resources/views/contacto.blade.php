5be0496feee0e8c4ba6c6c461455f561f3874b77
Mike <maik_kb@live.com.mx>
2018-07-11 12:08:08 -0500

    TERMINE DE MODIFICAR LAS RUTAS

--- probando-laravel/resources/views/contacto.blade.php
+++ probando-laravel/resources/views/contacto.blade.php
@@ -4,6 +4,6 @@
 	<title>FORM DE CONTACTO</title>
 </head>
 <body>
-
+	<h1> HOLA MUNDO {{$nombre}}</h1>
 </body>
 </html>
--- probando-laravel/routes/web.php
+++ probando-laravel/routes/web.php
@@ -15,7 +15,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+//REGRESA UN MENSAJE EN PANTALLA CON METODO GET
 Route::get('/hola-mundo', function() {
 	return 'Hola mundo!! soy mike ';
 });
+
+//REGRESA UN MENSAJE EN PANTALLA CON METODO POST
+Route::post('/hola-mundo', function() {
+	return 'Hola mundo!! soy mike WEB por post';
+});
+
+//REGRESA LA PAGINA CONTACTO EN PANTALLA CON METODO GET
+//Route::get('/contacto', function () {
+//   return view('contacto');
+// });
+
+//FUNCIONA CON VARIOS METODOS 
+//Route::match(['get','post'],'contacto', function() {
+//	return view('contacto');
+//});
+
+//FUNCIONA CON CUALQUIER METODO METODOS 
+//Route::any('contacto', function() {
+//	return view('contacto');
+//});
+
+//PASAR UN PARAMETRO A URL si quiero que sea opcional el envio de la variable se le agrega 
+// un signo de ?  y un valor por defecto 
+Route::get('/contacto/{nombre?}', function ($nombre = "Mike") {
+    return view('contacto',array(
+    	"nombre"=> $nombre
+    ));
+ });
